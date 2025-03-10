c6e37ad3941f878a0b922c66f7beb8477d965eb3
lsc713 <lsc7134@naver.com>
2025-03-10 12:53:59 +0900

    Docs : add logging setting

---
+++ finafanBE/src/main/java/com/a702/finafanbe/config/MetricsConfig.java
@@ -0,0 +1,24 @@
+package com.a702.finafanbe.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${spring.profiles.active:unknown}")
+    private String activeProfile;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+            .commonTags("application", applicationName)
+            .commonTags("environment", activeProfile);
+    }
+}
