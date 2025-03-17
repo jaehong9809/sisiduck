0c5746bd741c15cb811dea42206ce125bd9a27bb
Pearl-K <rkdwlswn227@gmail.com>
2025-03-17 17:42:21 +0900

    ✨ feat: chatbot 연관 데이터의 이동을 위한 data 계층 구현

---
+++ finafanAndroid/app/src/main/java/com/a702/finafan/data/chatbot/api/ChatApi.kt
@@ -0,0 +1,11 @@
+package com.a702.finafan.data.chatbot.api
+
+import com.a702.finafan.data.chatbot.dto.ChatRequest
+import com.a702.finafan.data.chatbot.dto.ChatResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ChatApi {
+    @POST("chat")
+    suspend fun sendMessage(@Body request: ChatRequest): ChatResponse
+}
---
+++ finafanAndroid/app/src/main/java/com/a702/finafan/data/chatbot/dto/ChatResponse.kt
@@ -0,0 +1,5 @@
+package com.a702.finafan.data.chatbot.dto
+
+data class ChatResponse(
+    val reply: String
+)
