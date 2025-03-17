fe70c23cfd75d502bc4807d9e0a7324dfd5771c2
lsc713 <lsc7134@naver.com>
2025-03-17 10:50:34 +0900

    add : jwtUtil constructure

--- finafanBE/src/main/java/com/a702/finafanbe/core/user/util/JwtUtil.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/user/util/JwtUtil.java
@@ -1,4 +1,31 @@
 package com.a702.finafanbe.core.user.util;
 
+import com.a702.finafanbe.core.user.entity.AuthTokens;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    private SecretKey secretKey;
+    private Long accessTokenExpiration;
+    private Long refreshTokenExpiration;
+
+    public JwtUtil(
+            @Value("jwt.secretkey") SecretKey secretKey,
+            @Value("jwt.access-expiration") Long accessTokenExpiration,
+            @Value("jwt.refresh-expiration") Long refreshTokenExpiration
+    ) {
+        this.secretKey = secretKey;
+        this.accessTokenExpiration = accessTokenExpiration;
+        this.refreshTokenExpiration = refreshTokenExpiration;
+    }
+
+
+
 }
