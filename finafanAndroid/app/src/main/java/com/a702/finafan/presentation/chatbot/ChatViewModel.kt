3d62896bbbeaf220d30623fde614fe6d7ee8336b
Pearl-K <rkdwlswn227@gmail.com>
2025-03-18 15:20:19 +0900

    ✨feat: HiltViewModel 설정 추가 및 RepositoryModule DI 설정 추가

--- finafanAndroid/app/src/main/java/com/a702/finafan/presentation/chatbot/ChatViewModel.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/presentation/chatbot/ChatViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.a702.finafan.common.utils.SpeechRecognizerHelper
 import com.a702.finafan.domain.chatbot.model.ChatMessage
 import com.a702.finafan.domain.chatbot.repository.ChatRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ChatViewModel @Inject constructor(
     private val repository: ChatRepository,
     private val speechRecognizerHelper: SpeechRecognizerHelper
