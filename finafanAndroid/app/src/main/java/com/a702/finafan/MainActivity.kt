44062ddd347ae2aa32d838fd686f11cae90fc255
Pearl-K <rkdwlswn227@gmail.com>
2025-03-18 17:10:10 +0900

    ✨feat: stt 테스트를 위한 디버깅 - MainActivity UI 영역 수정 - API 연결 임시 삭제

--- finafanAndroid/app/src/main/java/com/a702/finafan/MainActivity.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.a702.finafan.common.ui.theme.FinAFanTheme
@@ -19,7 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FinAFanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.safeDrawing)
+                ) { innerPadding ->
                     MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
