09a7a2c8686c5d7217609e67653b0b304b32e346
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-03-20 15:33:17 +0900

    ✨: API 로그 설정

---
+++ finafanBE/src/main/java/com/a702/finafanbe/core/chatbot/dto/response/AnswerResponse.java
@@ -0,0 +1,12 @@
+package com.a702.finafanbe.core.chatbot.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnswerResponse {
+    private String answer;
+}
