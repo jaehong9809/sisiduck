6e4bf9e8d2f30d74706841450bf5c9645cffd5fa
lsc713 <lsc7134@naver.com>
2025-03-20 17:00:40 +0900

    modify deploy properties

--- finafanBE/src/test/java/com/a702/finafanbe/FinafanBeApplicationTests.java
+++ finafanBE/src/test/java/com/a702/finafanbe/FinafanBeApplicationTests.java
@@ -3,11 +3,11 @@ package com.a702.finafanbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinafanBeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class FinafanBeApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
