8935c3be209f73923484e71d0a6ac34a05064521
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-03-25 01:04:28 +0900

    ✨ : 챗봇 프롬프트 분석

--- finafanAI/app/main.py
+++ finafanAI/app/main.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 api_key  = os.getenv("OPENAI_API_KEY")
 
-print(f"Loaded API Key: {api_key}")
 app = FastAPI(root_path="/ai")
 
 app.include_router(chatbot_router.router, prefix="/chatbot", tags=["Chatbot"])
-
 
 @app.get("/")
 def read_root():
