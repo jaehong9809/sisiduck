48e8ada7a9b44a82dc4427742dd49d39c598560b
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-03-24 11:20:13 +0900

    ✨: API 개발 (Stream X)

--- finafanAI/app/router/chatbot_router.py
+++ finafanAI/app/router/chatbot_router.py
@@ -2,12 +2,14 @@
 from fastapi import APIRouter, HTTPException
 from app.service.chatbot_service import question
 from app.schemas.question import QuestionRequest
+from fastapi.responses import StreamingResponse
+
 router = APIRouter()
 
 
 
 @router.post("/ask")
 async def ask_question(request: QuestionRequest):
-    # answer = question(request.question)
-    answer = "aa"
-    return {"response": answer}
+    answer = question(request.question)
+    # answer = "aa"
+    return answer
