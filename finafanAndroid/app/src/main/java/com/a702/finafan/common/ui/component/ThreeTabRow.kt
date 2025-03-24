ae65c6267e01d0137b97da3d06cce2d0849ca540
yejin <kimyejin0044@gmail.com>
2025-03-24 18:00:01 +0900

    ✨ feature : 공통 tabrow 컴포넌트 추가

---
+++ finafanAndroid/app/src/main/java/com/a702/finafan/common/ui/component/ThreeTabRow.kt
@@ -0,0 +1,99 @@
+package com.a702.finafan.common.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.a702.finafan.common.ui.theme.MainWhite
+
+@Composable
+fun ThreeTabRow(
+    labels: List<String>,
+    containerColor: Color,
+    selectedTabColor: Color,
+    onTabSelected: List<() -> Unit>,
+    modifier: Modifier = Modifier
+) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .background(color = MainWhite, shape = RoundedCornerShape(15.dp))
+        ) {
+        TabRow(
+            selectedTabIndex = selectedIndex,
+            containerColor = containerColor,
+            modifier = Modifier.fillMaxWidth(),
+            indicator = {},
+            divider = {}
+        ) {
+            labels.forEachIndexed { index, label ->
+                Tab(
+                    selected = selectedIndex == index,
+                    onClick = {
+                        selectedIndex = index
+                        onTabSelected[index].invoke()
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = if (selectedIndex == index) selectedTabColor else Color.Transparent,
+                            shape = RoundedCornerShape(15.dp)
+                        ),
+                    text = {
+                        Text(
+                            text = label,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            color = if (selectedIndex == index) Color.White else Color.Black // 텍스트 색상 설정
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
+
+// 여기부터는 테스트 코드입니다
+@Preview
+@Composable
+fun FundingScreen() {
+    ThreeTabRow(
+        labels = listOf("진행 중 모금", "참여 중 모금", "내가 만든 모금"),
+        containerColor = MainWhite,
+        selectedTabColor = Color.Blue,
+        onTabSelected = listOf(
+            { getAllFundings() },  // 첫 번째 탭: 모든 모금 조회
+            { getParticipatingFundings() },  // 두 번째 탭: 참여 중인 모금 조회
+            { getMyFundings() }  // 세 번째 탭: 내가 만든 모금 조회
+        )
+    )
+}
+
+fun getAllFundings() {
+    println("모든 모금 조회 API 호출")
+}
+
+fun getParticipatingFundings() {
+    println("참여 중인 모금 조회 API 호출")
+}
+
+fun getMyFundings() {
+    println("내가 생성한 모금 조회 API 호출")
+}
