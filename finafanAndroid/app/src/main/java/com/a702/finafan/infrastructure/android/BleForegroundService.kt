7e543baf5449f6de296366ea65c496d327f9edf8
Pearl-K <rkdwlswn227@gmail.com>
2025-03-27 09:57:31 +0900

    ✨feat: BleRepositoryEntryPoint 설정

--- finafanAndroid/app/src/main/java/com/a702/finafan/infrastructure/android/BleForegroundService.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/infrastructure/android/BleForegroundService.kt
@@ -4,10 +4,12 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.a702.finafan.di.entrypoint.BleRepositoryEntryPoint
 import com.a702.finafan.infrastructure.ble.BleAdvertiser
 import com.a702.finafan.infrastructure.ble.BleScanner
 import com.a702.finafan.infrastructure.util.BleUuidUtil
 import com.a702.finafan.infrastructure.util.NotificationFactory
+import dagger.hilt.android.EntryPointAccessors
 
 
 class BleForegroundService : Service() {
@@ -17,10 +19,15 @@ class BleForegroundService : Service() {
     override fun onCreate() {
         super.onCreate()
         advertiser = BleAdvertiser(this)
+
+        val repo = EntryPointAccessors.fromApplication(
+            applicationContext,
+            BleRepositoryEntryPoint::class.java
+        ).bleScanRepository()
+
         scanner = BleScanner(this) { scannedUuid ->
-
+            repo.addScannedUuid(scannedUuid)
         }
-
 
         val uuid = BleUuidUtil.getRotatingUuid(this)
         advertiser.start(uuid,
