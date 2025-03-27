8db3dabdb2e4afd7480f56630b64316db28b5da9
lsc713 <lsc7134@naver.com>
2025-03-27 14:10:22 +0900

    :recycle: feat: 상품 조회 관련 로직 구조를 변경하다

--- finafanBE/src/main/java/com/a702/finafanbe/core/demanddeposit/presentation/RetrieveDemandDepositController.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/demanddeposit/presentation/RetrieveDemandDepositController.java
@@ -1,11 +1,7 @@
 package com.a702.finafanbe.core.demanddeposit.presentation;
 
-import com.a702.finafanbe.core.demanddeposit.application.RetrieveAccountService;
-import com.a702.finafanbe.core.demanddeposit.dto.request.RetrieveProductsRequest;
 import com.a702.finafanbe.core.demanddeposit.dto.response.RetrieveProductsResponse;
-import com.a702.finafanbe.global.common.financialnetwork.util.FinancialNetworkUtil;
-import com.a702.finafanbe.global.common.financialnetwork.header.BaseRequestHeader;
-import com.a702.finafanbe.global.common.util.DateUtil;
+import com.a702.finafanbe.core.demanddeposit.facade.DemandDepositFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,29 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RetrieveDemandDepositController {
 
-    private final FinancialNetworkUtil financialNetworkUtil;
-    private final RetrieveAccountService retrieveAccountService;
+    private final DemandDepositFacade demandDepositFacade;
 
     @GetMapping("/products")
     public ResponseEntity<RetrieveProductsResponse> getDemandDepositList(
 //            @AuthMember User userId
     ) {
-        BaseRequestHeader retrieveDemandDepositRequestHeader = BaseRequestHeader.builder()
-                .apiName("inquireDemandDepositList")
-                .transmissionDate(DateUtil.getTransmissionDate())
-                .transmissionTime(DateUtil.getTransmissionTime())
-                .institutionCode(financialNetworkUtil.getInstitutionCode())
-                .fintechAppNo(financialNetworkUtil.getFintechAppNo())
-                .apiServiceCode("inquireDemandDepositList")
-                .institutionTransactionUniqueNo(financialNetworkUtil.getInstitutionTransactionUniqueNo())
-                .apiKey(financialNetworkUtil.getApiKey())
-                .build();
-
-        RetrieveProductsRequest retrieveDemandDepositRequest = new RetrieveProductsRequest(retrieveDemandDepositRequestHeader);
-        return retrieveAccountService.retrieveDemandDepositList(
-                "/demandDeposit/inquireDemandDepositList",
-                retrieveDemandDepositRequest
-        );
+        return demandDepositFacade.getProducts();
     }
 
 }
