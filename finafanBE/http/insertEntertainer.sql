732b8a774560563b4bd263d13fd5e7bb8a1ae6c6
lsc713 <lsc7134@naver.com>
2025-03-27 12:47:51 +0900

    :truck: chore: 임영웅으로 더미 변경

--- finafanBE/http/insertEntertainer.sql
+++ finafanBE/http/insertEntertainer.sql
@@ -1,5 +1,5 @@
 INSERT INTO entertainers (entertainer_id, entertainer_name, birth_date, entertainer_profile_url, fandom_name)
 VALUES
-    (1,'IU', '1993-05-16', 'https://example.com/profiles/iu', 'UAENA'),
+    (1,'임영웅', '1993-05-16', 'https://example.com/profiles/iu', '영웅시대'),
     (2,'BTS', '2013-06-13', 'https://example.com/profiles/bts', 'ARMY'),
     (3,'BLACKPINK', '2016-08-08', 'https://example.com/profiles/blackpink', 'BLINK');
