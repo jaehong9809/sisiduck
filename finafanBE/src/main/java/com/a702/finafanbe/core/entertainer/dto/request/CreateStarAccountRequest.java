3e64815967bd0d54f6d58a156cc15f7149f1122f
lsc713 <lsc7134@naver.com>
2025-03-27 17:31:27 +0900

    :art: chore: entertainer modify to Id

--- finafanBE/src/main/java/com/a702/finafanbe/core/entertainer/dto/request/CreateStarAccountRequest.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/entertainer/dto/request/CreateStarAccountRequest.java
@@ -2,7 +2,7 @@ package com.a702.finafanbe.core.entertainer.dto.request;
 
 public record CreateStarAccountRequest(
         String userEmail,
-        String entertainer,
+        Long entertainerId,
         String accountName
 ) {
 }
