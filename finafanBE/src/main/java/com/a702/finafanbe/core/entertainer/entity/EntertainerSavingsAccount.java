c5ac341f8d922e099cdf4946e98bfb42ef2e2d15
lsc713 <lsc7134@naver.com>
2025-03-28 17:39:29 +0900

    :truck: chore: accountId 저장방식 변경

--- finafanBE/src/main/java/com/a702/finafanbe/core/entertainer/entity/EntertainerSavingsAccount.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/entertainer/entity/EntertainerSavingsAccount.java
@@ -22,50 +22,38 @@ public class EntertainerSavingsAccount extends BaseEntity {
     @Column(nullable = false, name="user_id")
     private Long userId;
 
-    @Column(nullable = false, name ="account_name")
-    private String accountName;
+    @Column(nullable = false, name ="account_id")
+    private Long depositAccountId;
 
-    @Column(nullable = false, name = "deposit_account_no")
-    private String depositAccountNo;
+    @Column(nullable = false, name = "withdrawal_account_id")
+    private Long withdrawalAccountId;
 
-    @Column(nullable = false, name = "withdrawal_account_no")
-    private String withdrawalAccountNo;
+    @Column(nullable = false, name = "product_name")
+    private String productName;
 
     public static EntertainerSavingsAccount of(
             Long userId,
             Long entertainerId,
-            String accountName,
-            String depositAccountNo,
-            String withdrawalAccountNo
+            String productName,
+            Long withdrawalAccountId
     ) {
         return new EntertainerSavingsAccount(
                 userId,
                 entertainerId,
-                accountName,
-                depositAccountNo,
-                withdrawalAccountNo
+                productName,
+                withdrawalAccountId
         );
     }
 
     private EntertainerSavingsAccount(
             Long userId,
             Long entertainerId,
-            String accountName,
-            String depositAccountNo,
-            String withdrawalAccountNo
+            String productName,
+            Long withdrawalAccountId
     ){
         this.userId = userId;
         this.entertainerId = entertainerId;
-        this.accountName = accountName;
-        this.depositAccountNo = depositAccountNo;
-        this.withdrawalAccountNo = withdrawalAccountNo;
+        this.productName = productName;
+        this.withdrawalAccountId = withdrawalAccountId;
     }
 }
-//우리는 개인 적금이지만, 매일 출납이되야함.
-//적금은 매 날짜마다 출금이 가능하도록 되어있음.
-//기본적으로 출납을 하도록하되,
-//이자율을 우리가 래핑해서 활용해야함.
-//그럼 이자율에 따른 이자나 중도 해지에 대한 이자는. 우리가 자체적으로 제공해줘야함.
-//그럼 우리 계좌에서 이체를 하는데...
-
-//음.. 수시입출금이랑 연예인 적금이랑 동시에 만들고 사라지게 만들어야할 수 도 있겠는걸?
