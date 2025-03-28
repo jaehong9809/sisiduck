6f7bb1c8dfc251b60996afb0e29a769f325cf438
Pearl-K <rkdwlswn227@gmail.com>
2025-03-28 11:36:06 +0900

    ✨feat: BLE 기능 병합 후, NavGraph에 추가, 권한 설정 리팩토링

--- finafanAndroid/app/src/main/java/com/a702/finafan/presentation/main/MainScreen.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/presentation/main/MainScreen.kt
@@ -1,20 +1,28 @@
 package com.a702.finafan.presentation.main
 
+import android.Manifest
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.a702.finafan.common.ui.component.MainSquareIconButton
 import com.a702.finafan.common.ui.component.MainWideIconButton
 import com.a702.finafan.common.ui.theme.MainBlack
+import com.a702.finafan.infrastructure.android.BleForegroundService
+import com.a702.finafan.presentation.ble.rememberBlePermissionLauncher
 import com.a702.finafan.presentation.navigation.NavRoutes
 
 @Composable
@@ -22,6 +30,20 @@ fun MainScreen(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
+
+    val context = LocalContext.current
+
+    val blePermissionLauncher = rememberBlePermissionLauncher(
+        onGranted = {
+            val intent = Intent(context, BleForegroundService::class.java)
+            ContextCompat.startForegroundService(context, intent)
+            navController.navigate(NavRoutes.Ble.route)
+        },
+        onDenied = {
+            Toast.makeText(context, "권한이 거부되어 기능을 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+        }
+    )
+
     Column(modifier = modifier) {
         Row {
             MainSquareIconButton(
@@ -57,6 +79,30 @@ fun MainScreen(
                 text = "적금 가입"
             )
         }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        MainSquareIconButton(
+            onClick = {
+                blePermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.BLUETOOTH_ADVERTISE,
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                )
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Face,
+                    contentDescription = "Home",
+                    tint = MainBlack,
+                    modifier = Modifier.size(48.dp)
+                )
+            },
+            text = "주변 팬 찾기"
+        )
 
         Spacer(modifier = Modifier.padding(8.dp))
 
