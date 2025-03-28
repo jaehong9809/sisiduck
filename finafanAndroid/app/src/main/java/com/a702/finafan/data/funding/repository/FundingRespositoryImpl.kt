5fdfa63b52c6c9d59b6ac39ebfd40fda21fdb17e
yejin <kimyejin0044@gmail.com>
2025-03-28 13:15:44 +0900

    🔨 Chore: dummy 코드 삭제

--- finafanAndroid/app/src/main/java/com/a702/finafan/data/funding/repository/FundingRespositoryImpl.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/data/funding/repository/FundingRespositoryImpl.kt
@@ -22,49 +22,7 @@ class FundingRepositoryImpl @Inject constructor(
 ): FundingRepository {
 
     override suspend fun getFunding(fundingId: Long): FundingDetail {
-//        val response = api.getFundingDetail(fundingId)
-        val response = ApiResponse(
-            code = "S0000",
-            message = "정상적으로 처리되었습니다.",
-            data = FundingDetailResponse(
-                participated = false,
-                entertainer = Entertainer(
-                    entertainerId = 1, // 백엔드에서 ID 추가하는 게 맞는지 확인 필요
-                    name = "이찬원",
-                    imageUrl = "https://example.com/profile/chanwon.jpg"
-                ),
-                adminUser = AdminUser(
-                    adminName = "1",
-                    description = "이찬원을 사랑해서 커피를 나눠먹으려 합니다. 사랑해!",
-                    fundingCount = 7,
-                    fundingSuccessCount = 0
-                ),
-                fundingName = "커피차 서포트",
-                goalAmount = 2_000_000L,
-                currentAmount = 120_000L,
-                fundingExpiryDate = LocalDateTime.parse("2025-04-24T04:50:48.32062"),
-                fundingApplication = listOf(
-                    FundingApplication(
-                        name = "홍길동",
-                        balance = 20_000L,
-                        content = "내꺼야헤헤헤헤ㅋ.ㅋ",
-                        createdAt = LocalDateTime.parse("2025-03-27T13:31:06.236542")
-                    ),
-                    FundingApplication(
-                        name = "홍길동",
-                        balance = 20_000L,
-                        content = "내꺼야헤헤헤헤ㅋ.ㅋz.z",
-                        createdAt = LocalDateTime.parse("2025-03-27T13:31:11.124631")
-                    ),
-                    FundingApplication(
-                        name = "홍길동",
-                        balance = 20_000L,
-                        content = "내꺼야헤헤헤헤ㅋ.ㅋz.z!!",
-                        createdAt = LocalDateTime.parse("2025-03-27T13:31:14.251131")
-                    )
-                )
-            )
-        )
+        val response = api.getFundingDetail(fundingId)
         
         Log.d("getFundingDetail: ", "${response.data}")
 
@@ -76,25 +34,8 @@ class FundingRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getAllFundingList(): List<Funding> {
-//        val response = api.getAllFundingList()
-        val response = ApiResponse(
-            code = "S0000",
-            message = "정상적으로 처리되었습니다.",
-            data = listOf(
-                FundingResponse(
-                    entertainer = EntertainerResponse(
-                        entertainerId = 1,
-                        name = "이찬원",
-                        imageUrl = "https://a407-20250124.s3.ap-northeast-2.amazonaws.com/images/leechanwon.png"
-                    ),
-                    fundingId = 1,
-                    fundingName = "이찬원사랑해",
-                    currentAmount = 100000,
-                    goalAmount = 2000000,
-                    fundingExpiryDate = LocalDateTime.parse("2025-04-24T04:50:48.32062")
-                )
-            )
-        )
+        val response = api.getAllFundingList()
+
         Log.d("getAllFundingList: ", "${response.data}")
 
         return if (response.code == "S0000" && response.data != null) {
