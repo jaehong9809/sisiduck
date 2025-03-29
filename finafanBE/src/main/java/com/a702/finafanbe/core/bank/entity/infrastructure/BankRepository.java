80cff9e87c18274419f2eea8e93d15c4863f0e5c
lsc713 <lsc7134@naver.com>
2025-03-29 20:50:53 +0900

    :sparkles: feat : add bankEntity

--- finafanBE/src/main/java/com/a702/finafanbe/core/bank/entity/infrastructure/BankRepository.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/bank/entity/infrastructure/BankRepository.java
@@ -1,10 +1,12 @@
 package com.a702.finafanbe.core.bank.entity.infrastructure;
 
 import com.a702.finafanbe.core.bank.entity.Bank;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank,Long> {
 
+    Optional<Bank> findByBankId(Long bankId);
 }
