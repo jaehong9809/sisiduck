6397e5b8ce7f5a78a247db760a98c07344764e64
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-04-02 14:20:36 +0900

    🎨 feat: 적금 내역 상세 화면에 쓰이는 Gradient 수정

--- finafanAndroid/app/src/main/java/com/a702/finafan/common/ui/theme/Gradients.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/common/ui/theme/Gradients.kt
@@ -1,22 +1,46 @@
 package com.a702.finafan.common.ui.theme
 
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val gradientList = listOf(
 
-    Brush.radialGradient
-        (colors = listOf(Color(0xFF00C5DF), Color(0xFF907CFF), Color(0xFF18A0FB)),
-        radius = 4000f),
-    Brush.radialGradient(
-        colors = listOf(Color(0xFF18A0FB), Color(0xFF1BC47D), Color(0xFF00C5DF)),
-        radius = 4000f),
-    Brush.radialGradient(
-        colors = listOf(Color(0xFF18A0FB), Color(0xFF907CFF), Color(0xFF18A0FB)),
-        radius = 4000f),
-    Brush.radialGradient(
-        colors = listOf(Color(0xFF00C5DF), Color(0xFF18A0FB), Color(0xFF907CFF)),
-        radius = 4000f),
+    Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF907CFF),
+            Color(0xFF18A0FB),
+            Color(0xFF00C5DF)
+        ),
+        start = Offset(0f, 0f),
+        end = Offset(0f, 1000f)
+    ),
+    Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF5A2A6E),
+            Color(0xFF18A0FB),
+            Color(0xFF00C5DF)
+        ),
+        start = Offset(0f, 0f),
+        end = Offset(1000f, 2000f)
+    ),
+    Brush.linearGradient(
+        colors = listOf(
+            Color(0xFFA3E5FF),
+            Color(0xFF86D8B7)
+        ),
+        start = Offset(0f, 0f),
+        end = Offset(0f, 1200f)
+    ),
+    Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF002733),
+            Color(0xFF1069B0)
+        ),
+        start = Offset(0f, 0f),
+        end = Offset(0f, 1200f)
+    )
+
 )
 
 val gradientBlue = Brush.horizontalGradient(
