50baaed858af7f754ef9dc72cf4a78ae15214fd7
jyoooooung <jyoooooung@naver.com>
2025-04-06 22:37:00 +0900

    :wrench: 펀딩 입금 취소 List로 Dto에 담아서 여러건 취소 가능하게 변경

--- finafanBE/src/main/java/com/a702/finafanbe/core/funding/funding/presentation/FundingController.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/funding/funding/presentation/FundingController.java
@@ -95,14 +95,14 @@ public class FundingController {
     }
 
     // 펀딩 입금 취소
-    @DeleteMapping("/{fundingId}/withdraw/{fundingSupportId}")
+    @DeleteMapping("/{fundingId}/withdraw")
     public ResponseEntity<?> withdrawFunding(
             @PathVariable Long fundingId,
-            @PathVariable Long fundingSupportId
+            @RequestBody WithdrawTransactionRequest request
             //@AuthMember Long userId
     ) {
-        Long userId = 3L;
-        fundingService.withdrawFunding(userId, fundingId, fundingSupportId);
+        Long userId = 1L;
+        fundingService.withdrawFunding(userId, fundingId, request);
         return ResponseUtil.success();
     }
 
