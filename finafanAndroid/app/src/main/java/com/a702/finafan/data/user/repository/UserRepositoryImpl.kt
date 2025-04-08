03ffcc006db9414dc232c7f6192d6e59b0dcd3cb
Pearl-K <rkdwlswn227@gmail.com>
2025-04-08 10:59:46 +0900

    ✨feat: DataResource로 응답 Wrapping

--- finafanAndroid/app/src/main/java/com/a702/finafan/data/user/repository/UserRepositoryImpl.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/data/user/repository/UserRepositoryImpl.kt
@@ -1,14 +1,16 @@
 package com.a702.finafan.data.user.repository
 
+import com.a702.finafan.common.domain.DataResource
+import com.a702.finafan.common.utils.safeApiCall
 import com.a702.finafan.data.user.api.UserApi
 import com.a702.finafan.domain.user.model.User
 import com.a702.finafan.domain.user.repository.UserRepository
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val apiService : UserApi
+    private val userApi : UserApi
 ) : UserRepository {
-    override suspend fun getUserInfo(): User {
-        return apiService.getUserInfo().toDomain()
+    override suspend fun getUserInfo(): DataResource<User> {
+        return safeApiCall { userApi.getUserInfo().toDomain() }
     }
 }
