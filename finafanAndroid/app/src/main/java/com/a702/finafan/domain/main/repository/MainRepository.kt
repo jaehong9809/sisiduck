0cc19dea00fb8f2bf3e271d69a62b6cba96f1c83
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-04-09 09:33:34 +0900

    🔧 refactor: 메인 API 공통 response 사용

--- finafanAndroid/app/src/main/java/com/a702/finafan/domain/main/repository/MainRepository.kt
+++ finafanAndroid/app/src/main/java/com/a702/finafan/domain/main/repository/MainRepository.kt
@@ -1,11 +1,12 @@
 package com.a702.finafan.domain.main.repository
 
+import com.a702.finafan.common.domain.DataResource
 import com.a702.finafan.domain.main.model.MainRanking
 import com.a702.finafan.domain.main.model.MainSaving
 
 interface MainRepository {
-    suspend fun getMainSavings(): List<MainSaving>
-    suspend fun getDailyTop3(): List<MainRanking>
-    suspend fun getWeeklyTop3(): List<MainRanking>
-    suspend fun getTotalTop3(): List<MainRanking>
+    suspend fun getMainSavings(): DataResource<List<MainSaving>>
+    suspend fun getDailyTop3(): DataResource<List<MainRanking>>
+    suspend fun getWeeklyTop3(): DataResource<List<MainRanking>>
+    suspend fun getTotalTop3(): DataResource<List<MainRanking>>
 }
