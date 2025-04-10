3c3fd864855c6651f4a6df8dcf585a9cbee4a36d
jyoooooung <jyoooooung@naver.com>
2025-04-10 10:01:18 +0900

    :bug: 연예인 썸네일 사진 Response에 추가

--- finafanBE/src/main/java/com/a702/finafanbe/core/funding/funding/dto/EntertainerResponse.java
+++ finafanBE/src/main/java/com/a702/finafanbe/core/funding/funding/dto/EntertainerResponse.java
@@ -3,17 +3,20 @@ package com.a702.finafanbe.core.funding.funding.dto;
 public record EntertainerResponse (
         Long entertainerId,
         String name,
-        String imageUrl
+        String imageUrl,
+        String thumbnailUrl
 ) {
     public static EntertainerResponse of (
             Long entertainerId,
             String name,
-            String imageUrl
+            String imageUrl,
+            String thumbnailUrl
     ) {
         return new EntertainerResponse(
                 entertainerId,
                 name,
-                imageUrl
+                imageUrl,
+                thumbnailUrl
         );
     }
 }
